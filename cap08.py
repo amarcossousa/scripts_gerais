d1553ec7bbd7e34052978a884617d0e6f1ac5783
Marcos Sousa <a.marcos.car@bol.com.br>
2022-03-21 23:00:24 -0300

    Removida função de concatenação e adicion travessia com loop while

--- cap08.py
+++ cap08.py
@@ -1,2 +1,9 @@
-def escribe(a, b):
-    return a + b
+#FUNÇÃO DO LIVRO
+# Travessia com loop while
+
+fruit = 'banana'
+index = 0
+while index < len(fruit): # O loop exibe cada letra sozinha
+    letter = fruit[index] 
+    print(letter)
+    index = index +1
